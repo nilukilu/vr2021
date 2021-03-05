165ce5fff06a752ac88df6ca3cd03dd8150b2527
Nils Kunnapuu <nilskunnapuu@users.noreply.github.com>
2021-03-05 17:26:24 +0200

    Esimese tunni lopp

--- page.php
+++ page.php
@@ -7,6 +7,35 @@
     $semesterduration = $semesterbegin->diff($semesterend);
     $semesterdurationdays = $semesterduration->format("%r%a");
     $semesterdurhtml = "\n <p>2021 kevadsemester kestus on " .$semesterdurationdays ." päeva.</p> \n";
+    $today = new DateTime("now");
+    $fromsemesterbegin = $semesterbegin->diff($today);
+    $fromsemesterbegindays = $fromsemesterbegin->format("%r%a");
+
+    if($fromsemesterbegindays <= $semesterdurationdays){$semesterprogress = "\n" .'<p>Semester edeneb: <meter min="0" max="' .$semesterdurationdays .'" value="' .$fromsemesterbegindays .'">.</p>' ."\n";}
+    else {
+        $semesterprogress = "\n <p>Semester on lõppenud.</p> \n";
+    }
+
+    //loeme piltide kataloogi sisu
+    $picsdir = "../../pics/";
+    $allfiles = array_slice(scandir($picsdir), 2);
+    //echo $allfiles[0];
+
+    $photocount = count($picfiles);
+    $photonum = mt_rand(0, $photocount-1);
+    $randomphoto = $picfiles[$photonum];
+    $allowedphototypes = ["image/jpeg", "image/png"];
+
+    $picfiles = [];
+    foreach($allfiles as $file) {
+        $fileinfo = getimagesize($picsdir .$file);
+        // var_dump($fileinfo); edastab kogu info
+        if(isset($fileinfo[“mime”])) {
+            if(in_array($fileinfo[“mime”], $allowedphototypes)) {
+                array_push($picfiles, $file);
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -20,10 +49,12 @@
     <?php
         echo $myname;
         echo $semesterdurhtml;
+        echo $semesterprogress;
         ?>
 	<h1>Nils Künnapuu</h1>
     <?=$currenttime ?>
 	<p>See leht on valminud õppetöö raames!</p>
     <?=$timehtml ?>
+    <img src="<?php echo $picsdir .$randomphoto; ?>" alt="vaade Haapsalus">
 </body>
 </html>
